3c8732a4f2fbd0e1921c6c1858a7e4c9bfea0090
MICKAEL BERAFTA <mickael.berafta-prestataire@laposte.fr>
2018-08-03 13:51:14 +0200

    advection 1D improvements

--- public/controllers/MathController.js
+++ public/controllers/MathController.js
@@ -13,8 +13,8 @@ angular.module('main')
             scope.step = Math.abs($scope.xL - $scope.xR) / ($scope.totalPoints - 1);
             scope.mesh = mesh1D(scope.xL, scope.xR, scope.totalPoints, scope.step);
             scope.finalTime = 20;
-            scope.currentPoints = [];
-
+            scope.currentPoints = [new Point(0, 0)];
+            $scope.advectionSpeed = 3;
 
             scope.mesh.forEach((point, index) => {
                 if (index > 0 && index < $scope.mesh.length - 1)
@@ -28,8 +28,8 @@ angular.module('main')
             scope.canvas.style.background = "#34495e";
             scope.canvas.style.borderRadius = "10px";
             scope.canvas.style.transition = "0.250s";
-            scope.canvas.height = "300";
-            scope.canvas.width = "450";
+            scope.canvas.height = "600";
+            scope.canvas.width = "600";
 
             draw(scope.mesh);
             draw(scope.currentPoints);
@@ -38,17 +38,17 @@ angular.module('main')
                 dt = 0;
 
             let interval = setInterval(() => {
-                let CFL = dt / $scope.step < 1;
+                let CFL = ($scope.advectionSpeed * dt) / $scope.step < 1;
                 if (CFL) {
                     $scope.$apply(() => {
-                        transportSolver(dt);
+                        transportSolver(dt, $scope.advectionSpeed);
                     });
                     dt = 0.001 * (new Date().getTime() - time);
                 }
                 else {
                     clearInterval(interval);
                 }
-            }, 1000 / 20);
+            }, 1000 / 60);
 
         })($scope);
 
@@ -58,12 +58,12 @@ angular.module('main')
         function draw(pointsArray) {
             pointsArray.forEach((point, index) => {
                 let y = typeof (point) == 'object' ? $scope.canvas.height - 2 - point.y : $scope.canvas.height - 5,
-                    x = typeof (point) == 'object' ? point.x + 25 : point + 25;
+                    x = typeof (point) == 'object' ? point.x : point;
 
                 let newPoint = new Point(x, y);
                 $scope.ctx.fillStyle = typeof (point) == 'object' ? "#FFF125" : "#FFFFFF";
 
-                $scope.ctx.strokeStyle = "red";
+                $scope.ctx.strokeStyle = "green";
                 $scope.ctx.lineWidth = "1";
 
                 $scope.ctx.beginPath();
@@ -72,7 +72,7 @@ angular.module('main')
                 $scope.ctx.closePath();
 
                 if (index < $scope.mesh.length - 1) {
-                    let nextX = typeof (pointsArray[index]) == 'object' ? pointsArray[index + 1].x + 25 : pointsArray[index + 1] + 25,
+                    let nextX = typeof (pointsArray[index]) == 'object' ? pointsArray[index + 1].x + 50 : pointsArray[index + 1] + 25,
                         nextY = typeof (pointsArray[index]) == 'object' ? $scope.canvas.height - 2 - pointsArray[index + 1].y : $scope.canvas.height - 2;
                     $scope.ctx.beginPath();
                     $scope.ctx.moveTo(x, y);
@@ -84,8 +84,11 @@ angular.module('main')
             });
         }
 
-        function transportSolver(dt) {
+        function transportSolver(dt, a) {
+
+            // Réinitialisation de l'écran pour les nouveaux points
             $scope.ctx.clearRect(0, 0, $scope.canvas.width, $scope.canvas.height);
+
             for (let j = 0; j < $scope.mesh.length; j++) {
                 $scope.currentPoints[j].x = $scope.mesh[j];
 
@@ -93,14 +96,20 @@ angular.module('main')
                 if (j == 0)
                     console.log("Image 0 au nouveau temps dt : ", y);
 
-                let coeff = Number(dt / $scope.step),
-                    diff = j == 0 ? y : Number(-$scope.currentPoints[j - 1].y + y);
+                let coeff = a * Number(dt / $scope.step),
+                    diff;
+
+                // Schéma décentré amont ou aval selon la valeur de a
+                if (a > 0) {
+                    diff = j == 0 ? y : Number(-$scope.currentPoints[j - 1].y + y)
+                }
+                else {
+                    diff = j == $scope.mesh.length - 1 ? y : Number($scope.currentPoints[j + 1].y - y);
+                }
 
                 $scope.currentPoints[j].y = Number(y) - coeff * diff;
             }
-            $scope.computedResult.push($scope.currentPoints);
-            localStorage.setItem('computedResult', JSON.stringify($scope.computedResult));
-            console.log("dt / step = ", dt / $scope.step);
+
             draw($scope.currentPoints);
         }
 
@@ -124,7 +133,7 @@ angular.module('main')
         }
 
         function exp(point) {
-            return 150 * Math.exp(-Math.pow(point / 100, 2) / 2);
+            return 150 * Math.exp(-Math.pow(point / 100, 4) / 2);
         }
 
         function sin(point) {
