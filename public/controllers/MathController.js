b93525bd0a1eed038571343960953bb217200cac
MICKAEL BERAFTA <mickael.berafta-prestataire@laposte.fr>
2018-08-03 11:23:14 +0200

    advection 1D

--- public/controllers/MathController.js
+++ public/controllers/MathController.js
@@ -6,7 +6,8 @@ angular.module('main')
          */
 
         ((scope) => {
-            scope.totalPoints = 50;
+            $scope.computedResult = [];
+            scope.totalPoints = 100;
             scope.xL = 0;
             scope.xR = 1000;
             scope.step = Math.abs($scope.xL - $scope.xR) / ($scope.totalPoints - 1);
@@ -14,12 +15,13 @@ angular.module('main')
             scope.finalTime = 20;
             scope.currentPoints = [];
 
-            scope.mesh.forEach((point) => {
-                scope.currentPoints.push(new Point(point, 100 * Math.exp(-(point * point) / 10000)));
-            });
 
-            console.log('MESH POINTS --->', scope.currentPoints);
-            console.log('CURRENT POINTS --->', scope.currentPoints);
+            scope.mesh.forEach((point, index) => {
+                if (index > 0 && index < $scope.mesh.length - 1)
+                    scope.currentPoints.push(new Point(point, exp(point)));
+                else
+                    scope.currentPoints.push(new Point(point, 100));
+            });
 
             scope.canvas = document.getElementById('math');
             scope.ctx = scope.canvas.getContext('2d');
@@ -32,22 +34,21 @@ angular.module('main')
             draw(scope.mesh);
             draw(scope.currentPoints);
 
-            let time = new Date().getTime(),
-                dt = (new Date().getTime() - time) / 1000,
-                timeStep = 0;
+            var time = new Date().getTime(),
+                dt = 0;
+
             let interval = setInterval(() => {
-                let CFL = dt < 0.5 * $scope.step;
+                let CFL = dt / $scope.step < 1;
                 if (CFL) {
                     $scope.$apply(() => {
-                        transportSolver(timeStep);
+                        transportSolver(dt);
                     });
-                    timeStep += dt;
-                    console.log("TIMESTEP ->", timeStep);
+                    dt = 0.001 * (new Date().getTime() - time);
                 }
                 else {
                     clearInterval(interval);
                 }
-            }, 1000 / 60);
+            }, 1000 / 20);
 
         })($scope);
 
@@ -57,34 +58,49 @@ angular.module('main')
         function draw(pointsArray) {
             pointsArray.forEach((point, index) => {
                 let y = typeof (point) == 'object' ? $scope.canvas.height - 2 - point.y : $scope.canvas.height - 5,
-                    x = typeof (point) == 'object' ? point.x + 25 : point + 25,
-                    nextX = typeof (pointsArray[index]) == 'object' ? pointsArray[index].x + 25 : pointsArray[index] + 25,
-                    nextY = typeof (pointsArray[index]) == 'object' ? $scope.canvas.height - 2 - pointsArray[index].y : $scope.canvas.height - 2;
+                    x = typeof (point) == 'object' ? point.x + 25 : point + 25;
+
                 let newPoint = new Point(x, y);
                 $scope.ctx.fillStyle = typeof (point) == 'object' ? "#FFF125" : "#FFFFFF";
 
-                $scope.ctx.beginPath();
-                $scope.ctx.moveTo(x,y);
-                $scope.ctx.lineTo(nextX, nextY);
-                $scope.ctx.stroke();
+                $scope.ctx.strokeStyle = "yellow";
+                $scope.ctx.lineWidth = "1";
 
                 $scope.ctx.beginPath();
-                $scope.ctx.arc(newPoint.x, newPoint.y, 2, 0, 2 * Math.PI, false);
+                $scope.ctx.arc(x, y, 2, 0, 2 * Math.PI, false);
                 $scope.ctx.fill();
                 $scope.ctx.closePath();
+
+                if (index < $scope.mesh.length - 1) {
+                    let nextX = typeof (pointsArray[index]) == 'object' ? pointsArray[index + 1].x + 25 : pointsArray[index + 1] + 25,
+                        nextY = typeof (pointsArray[index]) == 'object' ? $scope.canvas.height - 2 - pointsArray[index + 1].y : $scope.canvas.height - 2;
+                    $scope.ctx.beginPath();
+                    $scope.ctx.moveTo(x, y);
+                    $scope.ctx.lineTo(nextX, nextY);
+                    $scope.ctx.stroke();
+                    $scope.ctx.closePath();
+                }
+
             });
         }
 
         function transportSolver(dt) {
-            let result = [];
             $scope.ctx.clearRect(0, 0, $scope.canvas.width, $scope.canvas.height);
-            for (let j = 0; j < $scope.mesh.length - 1; j++) {
+            for (let j = 0; j < $scope.mesh.length; j++) {
                 $scope.currentPoints[j].x = $scope.mesh[j];
 
                 let y = $scope.currentPoints[j].y;
-                $scope.currentPoints[j].y = y - (dt / $scope.step) * ($scope.currentPoints[j + 1].y - y);
-            }
+                if (j == 0)
+                    console.log("Image 0 au nouveau temps dt : ", y);
 
+                let coeff = Number(dt / $scope.step),
+                    diff = j == 0 ? y : Number(-$scope.currentPoints[j - 1].y + y);
+
+                $scope.currentPoints[j].y = Number(y) - coeff * diff;
+            }
+            $scope.computedResult.push($scope.currentPoints);
+            localStorage.setItem('computedResult', JSON.stringify($scope.computedResult));
+            console.log("dt / step = ", dt / $scope.step);
             draw($scope.currentPoints);
         }
 
@@ -105,6 +121,14 @@ angular.module('main')
                 result.push(xL + j * step);
             }
             return result;
+        }
+
+        function exp(point) {
+            return 150 * Math.exp(-Math.pow(point / 100, 2) / 2);
+        }
+
+        function sin(point) {
+            return 100 * Math.sin((point / 100) * Math.PI / 4);
         }
 
     }]);
